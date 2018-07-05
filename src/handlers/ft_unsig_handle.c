a2afa0e1dfe1c4b239adc8dc47fcc9c5952cdd13
Daniel Alexandrov <daniel.alexandrov.s@gmail.com>
2018-07-06 01:28:59 +0300

    changed files

--- src/handlers/ft_unsig_handle.c
+++ src/handlers/ft_unsig_handle.c
@@ -1,27 +1,5 @@
 #include "ft_printf.h"
 
-static uintmax_t convert(va_list *arg, t_tmp *tmp)
-{
-	uintmax_t	nbr;
-
-	nbr = va_arg(*arg, uintmax_t);
-	if (tmp->type == 1)
-		nbr = (unsigned char)nbr;
-	else if (tmp->type == 2)
-		nbr = (unsigned short int)nbr;
-	else if (tmp->type == 3)
-		nbr = (unsigned long int)nbr;
-	else if (tmp->type == 4)
-		nbr = (unsigned long long int)nbr;
-	else if (tmp->type == 5)
-		nbr = (uintmax_t)nbr;
-	else if (tmp->type == 6)
-		nbr = (size_t)nbr;
-	else
-		nbr = (unsigned int)nbr;
-	return (nbr);
-}
-
 static void ft_putunsig(uintmax_t nbr)
 {
 	char *num = "0123456789";
